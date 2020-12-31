6d85a572133d70980932540e52ee07ac4aea6da3
Ian Gow <iandgow@gmail.com>
2020-12-31 12:39:34 -0500

    Updated get_ff_ind.R.

--- get_ff_ind.R
+++ get_ff_ind.R
@@ -3,85 +3,63 @@
 # The idea is to make a table that could be used for SQL merges.
 ########################################################################
 
+library(DBI)
+library(dplyr, warn.conflicts = FALSE)
+library(tidyr)
+library(readr)
+
 # The URL for the data.
-ff.url <- paste("http://mba.tuck.dartmouth.edu", 
-                "pages/faculty/ken.french/ftp",
-                "Industry_Definitions.zip", sep="/")
-
-# Download the data and unzip it
-f <- tempfile() 
-download.file(ff.url, f) 
-file.list <- unzip(f,list=TRUE)
-
-trim <- function(string) {
-  # Remove leading and trailing spaces from a string
-  ifelse(grepl("^\\s*$", string, perl=TRUE),"", 
-         gsub("^\\s*(.*?)\\s*$","\\1",string,perl=TRUE))
+get_ff_ind <- function(num = 48) {
+  t <- tempfile(fileext = ".zip") 
+  
+  base_url <- paste0("https://mba.tuck.dartmouth.edu/",
+                     "pages/faculty/ken.french/ftp/Siccodes")
+  url <- paste0(base_url, num, ".zip")
+  
+  download.file(url, t)
+  
+  ff_data <- 
+    readr::read_fwf(t, 
+                    col_positions = readr::fwf_widths(c(3, 7, NA),
+                                                      c("ff_ind", 
+                                                        "ff_ind_short_desc", 
+                                                        "sic_range")),
+                    col_types = "icc") %>%
+    mutate(ff_ind_desc = if_else(!is.na(ff_ind_short_desc), 
+                                 sic_range, NA_character_)) %>%
+    tidyr::fill(ff_ind, ff_ind_short_desc, ff_ind_desc) %>%
+    filter(grepl("^[0-9]", sic_range)) %>%
+    tidyr::extract(sic_range, 
+                   into = c("sic_min", "sic_max", "sic_desc"),
+                   regex = "^([0-9]+)-([0-9]+)(.*)$",
+                   convert = TRUE) %>%
+    mutate(sic_desc = trimws(sic_desc))
+  unlink(t)
+  ff_data
 }
 
-# Function to do the heavy lifting
-extract_ff_ind_data <- function (file) {
+write_ff_table <- function(i) {
+  pg <- dbConnect(RPostgres::Postgres())
   
-  # Read in the data in a plain form
-  z <- unzip(f, files=file)
-  ff_ind <- as.vector(read.delim(z, header=FALSE, 
-                                 stringsAsFactors=FALSE))
-  unlink(z)
-  # The first 10 characters of each line are the industry data, but only the first
-  # row of the data for the SIC codes in an industry are filled in;
-  # so fill in the rest.
-  ind_num <- trim(substr(ff_ind[,1],1,10))
-  for (i in 2:length(ind_num)) { 
-    if (ind_num[i]=="") ind_num[i] <- ind_num[i-1]
-  }
+  dbExecute(pg, "SET search_path TO ff")
   
-  # The rest of each line is either detail on an industry or details about the
-  # range of SIC codes that fit in each industry with a label for each group
-  # of SIC codes.
-  sic_detail <- trim(substr(ff_ind[,1],11,100))
+  get_ff_ind(i) %>% 
+    dbWriteTable(pg, paste0("ind_", i), .,
+                 overwrite=TRUE, row.names=FALSE)
   
-  # If the line doesn't start with a number, it's an industry description
-  is.desc <- grepl("^\\D",sic_detail,perl=TRUE)
+  rs <- dbExecute(pg, paste0("VACUUM ind_", i))
+  rs <- dbExecute(pg, paste0("CREATE INDEX ON ind_", i,
+                              " (ff_ind)"))
   
-  # Pull out information from rows about industries
-  regex.ind <- "^(\\d+)\\s+(\\w+).*$"
-  ind_num <- gsub(regex.ind,"\\1",ind_num,perl=TRUE)
-  ind_abbrev <- gsub(regex.ind,"\\2",ind_num[is.desc],perl=TRUE)
-  ind_list <- data.frame(ind_num=ind_num[is.desc],ind_abbrev, 
-                         ind_desc=sic_detail[is.desc])
-  
-  # Pull out information rows about ranges of SIC codes
-  regex.sic <- "^(\\d+)-(\\d+)\\s*(.*)$"
-  ind_num <- ind_num[!is.desc]
-  sic_detail <- sic_detail[!is.desc]
-  sic_low  <- as.integer(gsub(regex.sic,"\\1",sic_detail,perl=TRUE))
-  sic_high <- as.integer(gsub(regex.sic,"\\2",sic_detail,perl=TRUE))
-  sic_desc <- gsub(regex.sic,"\\3",sic_detail,perl=TRUE)
-  sic_list <- data.frame(ind_num, sic_low, sic_high, sic_desc)
-  
-  return(merge(ind_list,sic_list,by="ind_num",all=TRUE))
-}
-
-# Load the data into my database
-library(RPostgreSQL)
-pg <- dbConnect(PostgreSQL())
-# Extract the data of interest
-for (i in c(12, 17, 48, 49)) {
-  temp <- extract_ff_ind_data(paste0("Siccodes", i , ".txt"))
-  rs <- dbWriteTable(pg,c("ff",paste0("ind_", i)), temp,
-                     overwrite=TRUE, row.names=FALSE)
-  
-  rs <- dbGetQuery(pg, paste0("VACUUM ff.ind_", i))
-  rs <- dbGetQuery(pg, paste0("CREATE INDEX ON ff.ind_", i,
-                              " (ind_num)"))
-  
-  dbExecute(pg, paste0("ALTER TABLE ff.ind_", i, " OWNER TO ff"))
-  dbExecute(pg, paste0("GRANT SELECT ON ff.ind_", i, " TO ff_access"))
+  dbExecute(pg, paste0("ALTER TABLE ind_", i, " OWNER TO ff"))
+  dbExecute(pg, paste0("GRANT SELECT ON ind_", i, " TO ff_access"))
   
   sql <- paste0("
-    COMMENT ON TABLE ff.ind_", i, " IS
+    COMMENT ON TABLE ind_", i, " IS
     'CREATED USING get_ff_ind.R ON ", Sys.time() , "';")
-  rs <- dbGetQuery(pg, paste(sql, collapse="\n"))
+  rs <- dbExecute(pg, paste(sql, collapse="\n"))
+  rs <- dbDisconnect(pg)
 }
 
-rs <- dbDisconnect(pg)
+# Get Fama-French industry data
+lapply(c(12, 17, 48, 49L), write_ff_table)
