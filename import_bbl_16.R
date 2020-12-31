c89fe3e9fbfb5490e64ee94f0f81675742ec78d4
Ian Gow <iandgow@gmail.com>
2020-12-31 12:39:12 -0500

    Tweaked code for bbl_16.R.

--- import_bbl_16.R
+++ import_bbl_16.R
@@ -1,18 +1,21 @@
-bbl_16 <- read.csv("bbl_16.csv", as.is=TRUE, row.names=NULL)
+library(readr)
+library(DBI)
+bbl_16 <- read_csv("bbl_16.csv", col_types="icii")
 
 # Connect to my database
-library(RPostgreSQL)
-pg <- dbConnect(PostgreSQL())
-dbWriteTable(pg, c("ff", "bbl_16"), bbl_16, overwrite=TRUE, row.names=FALSE)
+pg <- dbConnect(RPostgres::Postgres())
+
+rs <- dbExecute(pg, "SET search_path TO ff")
+dbWriteTable(pg, "bbl_16", bbl_16, overwrite=TRUE, row.names=FALSE)
 
 sql <- paste0("
     COMMENT ON TABLE ff.bbl_16 IS
     'CREATED USING get_bbl_16.R ON ", Sys.time() , "';")
 rs <- dbExecute(pg, paste(sql, collapse="\n"))
 
-rs <- dbExecute(pg, "VACUUM ff.bbl_16")
+rs <- dbExecute(pg, "VACUUM bbl_16")
 
-dbExecute(pg, "ALTER TABLE ff.bbl_16 OWNER TO ff")
-dbExecute(pg, "GRANT SELECT ON ff.bbl_16 TO ff_access")
+dbExecute(pg, "ALTER TABLE bbl_16 OWNER TO ff")
+dbExecute(pg, "GRANT SELECT ON bbl_16 TO ff_access")
 
 rs <- dbDisconnect(pg)
