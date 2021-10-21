639c6a2231b20c69b8f76655fa8faf287ff1f678
Ian Gow <iandgow@gmail.com>
2021-10-21 09:10:05 -0400

    Added missing file to script.

--- update_ff.sh
+++ update_ff.sh
@@ -3,6 +3,7 @@ psql -c "CREATE SCHEMA IF NOT EXISTS ff"
 
 Rscript import_bbl_16.R
 Rscript get_ff_ind.R
+Rscript get_ff_ind_rets_monthly.R
 Rscript get_ff_factors_daily.R
 Rscript get_ff_factors_monthly.R
 Rscript import_be_beme.R
